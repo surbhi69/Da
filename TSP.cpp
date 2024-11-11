d356a62ce920e4920a8562b5c8c8a9dc35bc37d6
AMOL  MARATHE <138742505+surbhi69@users.noreply.github.com>
2024-11-11 20:07:54 +0530

    Update TSP.cpp

--- TSP.cpp
+++ TSP.cpp
@@ -1,58 +1,94 @@
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <climits>
+using namespace std;
 
-int main() {
-    int n;
-    printf("Enter the number of processes: ");
-    scanf("%d", &n);
+#define INF 999
 
-    int arrival[n], burst[n], remaining[n], waiting[n], turnaround[n], completed = 0, time = 0;
-    float total_wait = 0, total_turnaround = 0;
+class Graph {
+    int w[20][20], n;
+    string city[20];
+    vector<int> bestPath;
+    int min_cost = INT_MAX;
 
-    for (int i = 0; i < n; i++) {
-        printf("Enter arrival and burst time for process P%d: ", i + 1);
-        scanf("%d %d", &arrival[i], &burst[i]);
-        remaining[i] = burst[i];
+public:
+    void initialize() {
+        cout << "Enter number of cities:\n";
+        cin >> n;
+        cout << "Enter city names:\n";
+        for (int i = 0; i < n; i++) cin >> city[i];
+        
+        cout << "Enter weighted matrix:\n";
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++) {
+                cout << "Enter distance between " << city[i] << " and " << city[j] << " (999 if none): ";
+                cin >> w[i][j];
+                if (i == j) w[i][j] = 0;
+            }
     }
 
-    printf("\nGantt Chart:\n");
+    void TSP() {
+        vector<int> currPath = {0};
+        bool visited[20] = {false};
+        visited[0] = true;
+        tspRecursive(0, visited, 1, 0, currPath);
+        
+        cout << "Minimum cost: " << min_cost << "\nPath: ";
+        for (int i : bestPath) cout << city[i] << " -> ";
+        cout << city[0] << endl;
+    }
 
-    while (completed != n) {
-        int min_index = -1, min_time = 9999;
-
+private:
+    int reduceMatrix(int matrix[20][20], bool isRow) {
+        int reductionCost = 0;
         for (int i = 0; i < n; i++) {
-            if (arrival[i] <= time && remaining[i] > 0 && remaining[i] < min_time) {
-                min_time = remaining[i];
-                min_index = i;
+            int minVal = INF;
+            for (int j = 0; j < n; j++) 
+                minVal = min(minVal, isRow ? matrix[i][j] : matrix[j][i]);
+            
+            if (minVal != INF && minVal != 0) {
+                reductionCost += minVal;
+                for (int j = 0; j < n; j++) 
+                    if (isRow) matrix[i][j] -= minVal;
+                    else matrix[j][i] -= minVal;
             }
         }
-
-        if (min_index == -1) {
-            time++;
-            continue;
-        }
-
-        remaining[min_index]--;
-        printf("P%d ", min_index + 1);
-
-        if (remaining[min_index] == 0) {
-            completed++;
-            int finish_time = time + 1;
-            turnaround[min_index] = finish_time - arrival[min_index];
-            waiting[min_index] = turnaround[min_index] - burst[min_index];
-            total_wait += waiting[min_index];
-            total_turnaround += turnaround[min_index];
-        }
-
-        time++;
+        return reductionCost;
     }
 
-    printf("\n\nProcess\tArrival\tBurst\tWaiting\tTurnaround\n");
-    for (int i = 0; i < n; i++) {
-        printf("P%d\t%d\t%d\t%d\t%d\n", i + 1, arrival[i], burst[i], waiting[i], turnaround[i]);
+    void tspRecursive(int pos, bool visited[], int count, int cost, vector<int>& path) {
+        if (count == n) {
+            int totalCost = cost + w[pos][0];
+            if (totalCost < min_cost) {
+                min_cost = totalCost;
+                bestPath = path;
+            }
+            return;
+        }
+
+        int tempMatrix[20][20];
+        copy(&w[0][0], &w[0][0] + 20 * 20, &tempMatrix[0][0]);
+        for (int i = 0; i < n; i++) if (visited[i]) for (int j = 0; j < n; j++) tempMatrix[i][j] = INF;
+
+        int lowerBound = cost + reduceMatrix(tempMatrix, true) + reduceMatrix(tempMatrix, false);
+        if (lowerBound >= min_cost) return;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i] && w[pos][i] != INF) {
+                visited[i] = true;
+                path.push_back(i);
+                tspRecursive(i, visited, count + 1, cost + w[pos][i], path);
+                path.pop_back();
+                visited[i] = false;
+            }
+        }
     }
+};
 
-    printf("\nAverage Waiting Time: %.2f", total_wait / n);
-    printf("\nAverage Turnaround Time: %.2f\n", total_turnaround / n);
-
+int main() {
+    Graph g;
+    g.initialize();
+    g.TSP();
     return 0;
 }
