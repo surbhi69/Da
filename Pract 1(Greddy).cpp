c800e76e3e7e11893d3730f1d8135a540928c5d7
AMOL  MARATHE <138742505+surbhi69@users.noreply.github.com>
2024-11-11 16:28:56 +0530

    Update Pract 1(Greddy).cpp

--- Pract 1(Greddy).cpp
+++ Pract 1(Greddy).cpp
@@ -4,47 +4,61 @@
 using namespace std;
 
 struct Item {
-    int profit, weight;
-    double ratio;
+    int profit;
+    int weight;
+    double fraction;
+    int index; // To track original index for output
 };
 
-bool compare(const Item& a, const Item& b) {
-    return a.ratio > b.ratio;
+// Function to compare items by their profit/weight ratio
+bool compare(Item a, Item b) {
+    return a.fraction > b.fraction;
 }
 
 int main() {
-    int capacity, n;
-    cout << "Enter knapsack capacity and number of items: ";
-    cin >> capacity >> n;
+    int w, n;
+
+    cout << "Enter the capacity of knapsack (weight): ";
+    cin >> w;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
 
     vector<Item> items(n);
-    for (int i = 0; i < n; ++i) {
-        cout << "Enter profit and weight for item " << i + 1 << ": ";
+    vector<double> value(n, 0.0); // Initialize value array to 0 for fractions
+
+    for (int i = 0; i < n; i++) {
+        cout << "Enter Profit and Weight for item " << i + 1 << ":\n";
         cin >> items[i].profit >> items[i].weight;
-        items[i].ratio = double(items[i].profit) / items[i].weight;
+        items[i].fraction = double(items[i].profit) / double(items[i].weight);
+        items[i].index = i;
     }
 
+    // Sort items by profit-to-weight ratio in descending order
     sort(items.begin(), items.end(), compare);
 
     double maxProfit = 0.0;
-    vector<double> fractions(n, 0.0);
 
-    for (int i = 0; i < n && capacity > 0; ++i) {
-        if (capacity >= items[i].weight) {
-            capacity -= items[i].weight;
+    for (int i = 0; i < n; i++) {
+        if (w >= items[i].weight) {
+            w -= items[i].weight;
             maxProfit += items[i].profit;
-            fractions[i] = 1.0;
+            value[items[i].index] = 1.0; // Full item taken
         } else {
-            fractions[i] = double(capacity) / items[i].weight;
-            maxProfit += items[i].profit * fractions[i];
+            // Take a fraction of the item
+            maxProfit += items[i].profit * ((double) w / items[i].weight);
+            value[items[i].index] = (double) w / items[i].weight;
+            w = 0; // Knapsack is now full
             break;
         }
     }
 
-    cout << "\nMax Profit: " << maxProfit << "\nFractions of items used:\n";
-    for (int i = 0; i < n; ++i) {
-        cout << "Item " << i + 1 << ": " << fractions[i] << "\n";
+    // Output results
+    cout << "\nMax Profit: " << maxProfit << "\n";
+    cout << "Fraction of each item used:\n";
+    for (int i = 0; i < n; i++) {
+        cout << "Item " << i + 1 << ": " << value[i] << "\n";
     }
 
     return 0;
-}
+}  
